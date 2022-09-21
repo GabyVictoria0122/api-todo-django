ede79dc5aad3a176bea0be628a53d24cfa6a396d
Gabriela Victoria <gabrielavictoria91@gmail.com>
2022-09-21 11:38:20 -0300

    arrumando lint

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
+
 def api_tasks(request):
     return JsonResponse()
+
 
 def api_user(request):
     return JsonResponse()
